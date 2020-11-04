7d79121c3cbb7b23a5fed5d2271c1d45b7d64024
张飞车 <2088682981@qq.com>
2020-11-05 07:28:18 +0800

    feat(error handle): error handle
    BREAKING CHANGE: error handle

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,5 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { LOADIPHLPAPI } from 'dns'
-import { processHeaders } from './helpers/headers'
-import { AxiosResponse } from './types/index'
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
+import axios from './axios'
 
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  // 必须在data前面因为根据没处理的data处理headers
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+export * from './types'
 
 export default axios
